38b3bf23976eb44210e98e7005829d5e70067426
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-06-20 10:22:58 +0530

    added hello

--- day27   19/teacher.py
+++ day27   19/teacher.py
@@ -1,13 +1,16 @@
-import threading 
-import time
-def student(name):
-    print(name,"started exam")
-    time.sleep(3)
-    print(name,"submitted paper")
-t1 = threading.Thread(target=student,args=("bala",),name=("student-1"))
-t2 = threading.Thread(target=student,args=("nani",),name=("student-2"))
-t1.start()    
-t2.start()
-t1.join()
-t2.join()
-print("teacher collected all papers")
+# import threading 
+# import time
+# def student(name):
+#     print(name,"started exam")
+#     time.sleep(3)
+#     print(name,"submitted paper")
+# t1 = threading.Thread(target=student,args=("bala",),name=("student-1"))
+# t2 = threading.Thread(target=student,args=("nani",),name=("student-2"))
+# t1.start()    
+# t2.start()
+# t1.join()
+# t2.join()
+# print("teacher collected all papers")
+
+
+print("hello")
